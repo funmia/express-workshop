e36e6dce285454946c94a486de14af7ac5cd6416
Oluwafunmilayo Adewodu <funmiadewodu@yahoo.com>
2017-08-16 23:13:30 +0100

    sending data to the server through a post request

--- server.js
+++ server.js
@@ -1,7 +1,14 @@
 var express = require('express');
 var app = express();
+var formidable = require('express-formidable');
 
 app.use(express.static("public"));
+app.use(formidable());
+
+
+app.post("/create-post", function(req, res) {
+  console.log(req.fields);
+});
 
 app.listen(3000, function () {
   console.log('Server is listening on port 3000. Ready to accept requests!');
