5fe5b29caf48704ab3bb4f935d132a30da44009e
Oluwafunmilayo Adewodu <funmiadewodu@yahoo.com>
2017-08-16 22:53:17 +0100

    serving static files in public dir

--- server.js
+++ server.js
@@ -1,15 +1,7 @@
 var express = require('express');
 var app = express();
 
-app.get("/", function(req, res){
-  res.send("Yay Node Girls!");
-});
-
-app.get("/chocolate", function(req, res){
-  res.send("I love chocolate!")
-});
-
-
+app.use(express.static("public"));
 
 app.listen(3000, function () {
   console.log('Server is listening on port 3000. Ready to accept requests!');
